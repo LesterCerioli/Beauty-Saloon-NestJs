d9a661ac7e9fa70eb7306ef93c94a92a05cd9560
unknown <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-08-01 06:55:03 -0300

    Building models and DTOs

--- src/attendants/entities/attendant.entity.ts
+++ src/attendants/entities/attendant.entity.ts
@@ -1,1 +1,13 @@
-export class Attendant {}
+// src/attendant/attendant.entity.ts
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsString } from 'class-validator';
+
+@Entity('attendants')
+export class Attendant {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', nullable: false })
+  @IsString()
+  attendantName: string;
+}
