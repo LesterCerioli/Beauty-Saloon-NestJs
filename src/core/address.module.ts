4e2027a022b08b5bce6716c9a00881cfe1718b83
unknown <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-08-03 05:57:18 -0300

    feat(#1785): Building configuration for Swagger

--- src/core/address.module.ts
+++ src/core/address.module.ts
@@ -1,11 +1,14 @@
+// src/core/services/addresses/address.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Address } from 'src/core/entities/address.entity';
 import { AddressService } from './services/addresses/address.service';
-
 import { AddressController } from './address.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Address])],
   controllers: [AddressController],
   providers: [AddressService],
-  exports: [AddressService], // Se outros módulos também precisarem deste serviço
+  exports: [AddressService], // Se você precisar usar o AddressService em outros módulos
 })
 export class AddressModule {}
--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,10 +1,12 @@
+// src/core/core.module.ts
 import { Module } from '@nestjs/common';
+import { AddressModule } from './address.module';
 import { CoreService } from './core.service';
 import { CoreController } from './core.controller';
-import { AddressController } from './address.controller'; // Corrigido
 
 @Module({
-  controllers: [AddressController, CoreController], // Corrigido
+  imports: [AddressModule], // Certifique-se de importar o módulo correto
+  controllers: [CoreController],
   providers: [CoreService],
 })
 export class CoreModule {}
